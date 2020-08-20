b8619a45747fd9f02d1c63171822364373f1ee5d
Mitya <techwipes@gmail.com>
2020-08-20 15:50:28 +0500

    self made exception

--- src/Lesson27/Test12.java
+++ src/Lesson27/Test12.java
@@ -1,20 +1,20 @@
 package Lesson27;
 
 public class Test12 {
-        void marafon(int tempAir, int tempRunning) throws LegsInguryException{
-            if(tempRunning > 12){
-                throw new LegsInguryException("Oups! Too fast!" );
-            }
-            if(tempAir > 35){
-                throw new ContractedMuscleException();
-            }
-            System.out.println("Finished marafon");
+    void marafon(int tempAir, int tempRunning) throws LegsInguryException {
+        if (tempRunning > 12) {
+            throw new LegsInguryException("Oups! Too fast!");
         }
+        if (tempAir > 35) {
+            throw new ContractedMuscleException();
+        }
+        System.out.println("Finished marafon");
+    }
 
     public static void main(String[] args) {
         Test12 t12 = new Test12();
         try {
-            t12.marafon(141,11);
+            t12.marafon(141, 11);
         } catch (LegsInguryException e) {
             System.out.println(e.getMessage());
         }
@@ -28,7 +28,8 @@ class LegsInguryException extends Exception {
         super(message);
     }
 
-    LegsInguryException() {}
+    LegsInguryException() {
+    }
 }
 
 
